56d8b1d0cc1b049b57072e35aa9863734c725b83
Daniela <somosjugandounrato@gmail.com>
2025-11-07 19:15:43 -0300

    insertar sliders en columnas para las habilidades

--- app.py
+++ app.py
@@ -8,4 +8,13 @@ with st.sidebar:
     st.markdown("Ajusta las habilidades de tu personaje (0-30)")
     
     col_str , col_dex = st.columns(2)
-    
+    col_con , col_int = st.columns(2)
+    col_wis , col_cha = st.columns(2)
+
+columnas_habilidades = {"Fuerza" : col_str, "Destreza": col_dex , "Constitución" : col_con , "Inteligencia" : col_int ,
+                        "Sabiduría" : col_wis , "Carisma" : col_cha}
+
+for habilidad in columnas_habilidades:
+    with columnas_habilidades[habilidad]:
+        valor = st.slider(f"{habilidad}" , 1 , 30 , 10 , key= f"sl{habilidad}" , help=f"Puntuación base de: {habilidad}")
+        #CALCULAR BONO
